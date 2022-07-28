5515589276f3f8d2a3b218ab6aa6f9585aced925
Julian Aijal <julian@thenextweb.com>
2022-07-28 17:54:14 +0200

    add test script

--- pages/posts/post-one.js
+++ pages/posts/post-one.js
@@ -1,5 +1,6 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function Post() {
   return (
@@ -7,6 +8,13 @@ export default function Post() {
       <Head>
         <title>Post One</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`FB script loaded`)
+        }
+      />
       <h1>Post one</h1>
       <Link href='/'>
           <a>{'<'} Back</a>
