3d039a9349a834b5697bca1a8af0af9a36805ae2
Julian Aijal <julian@thenextweb.com>
2022-07-28 18:03:04 +0200

    change img

--- pages/index.js
+++ pages/index.js
@@ -65,12 +65,12 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          <img src="../images/elmo.jpg" alt="elmo" className="logo" />
         </a>
       </footer>
 
