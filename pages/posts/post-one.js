8c2e3d56fb22144c9f1719f355b9c4593ce1571d
Julian Aijal <julian@thenextweb.com>
2022-07-28 17:02:15 +0200

    add head component to post

--- pages/posts/post-one.js
+++ pages/posts/post-one.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head';
 
 export default function Post() {
   return (
     <>
+      <Head>
+        <title>Post One</title>
+      </Head>
       <h1>Post one</h1>
       <Link href='/'>
           <a>{'<'} Back</a>
