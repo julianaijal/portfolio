91d97ec4b62ba94810bf49909ae5c36a7a1242ff
Julian Aijal <julian@thenextweb.com>
2022-07-31 23:13:21 +0200

    fix unconfigured host issue

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+        images: {
+          domains: [
+            'julianaijal.com',
+        ],
+        },    
+  }
+
+
+  module.exports = nextConfig
