8079af21c922ec04907440179c35ef108d273246
Julian Aijal <julian@thenextweb.com>
2022-08-02 21:05:57 +0200

    remove util styles and module

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 // https://portfolio-flax-three-97.vercel.app
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
@@ -18,7 +17,7 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section>
         <p className='text-center'>
           (This is a sample website on{' '}
           <a href="https://nextjs.org/learn">Next.js</a>.)
