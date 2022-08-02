7d0d53a36d3905dbba343a18c27692f2598bdf80
Julian Aijal <julian@thenextweb.com>
2022-08-02 20:15:12 +0200

    remove img ternary

--- components/layout.js
+++ components/layout.js
@@ -31,39 +31,9 @@ export default function Layout({ children, home }) {
       </Head>
       <TagmanagerIframe />
       <Header>
-        {home ? (
           <>
-            <Image
-              priority
-              // src="/../public/images/elmo-fire.gif"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  // src="/../public/images/elmo-fire.gif"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
       </Header>
       <main>{children}</main>
       {!home && (
