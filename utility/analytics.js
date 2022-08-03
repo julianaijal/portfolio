a7b690913df25b0dec91ba1f8bd2ff4a55ea4161
Julian Aijal <julian@thenextweb.com>
2022-08-03 13:14:21 +0200

    implement gtm via npm package

--- utility/analytics.js
+++ utility/analytics.js
@@ -1,24 +1,17 @@
-export function TagmanagerFunction() {
+import Analytics from 'analytics'
+import googleTagManager from '@analytics/google-tag-manager'
+const GTM_CONTAINER_ID = 'GTM-M8PS5F6'
 
-  return  <script
-    dangerouslySetInnerHTML={{
-      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-M8PS5F6');`,
-    }}
-    />
-  ;
-}
+const analytics = Analytics({
+    app: 'hallometmij',
+    plugins: [
+        googleTagManager({
+            containerId: GTM_CONTAINER_ID,
+						enabled: true,
+        }),
+    ]
+})
+
+export default analytics
 
 
-export function TagmanagerIframe() {
-  return <noscript
-  dangerouslySetInnerHTML = {
-    {
-      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8PS5F6" height="0" width="0" style="display: none; visibility: hidden;" />`,
-    }
-  }
-  />
-}
