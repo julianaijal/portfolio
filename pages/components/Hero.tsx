b92d71f3987ef7c5d1eb31ed69f72152211fdd73
Julian Aijal <julian@thenextweb.com>
2023-08-10 14:06:41 +0200

    set hero grid

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -1,9 +1,20 @@
 import styles from '../../styles/Hero.module.scss';
+import Image from 'next/image';
 const Hero = () => {
   return (
-    <h1 className={styles.title}>
-      Welcome to <a href="#">julianaijal.com</a>
-    </h1>
+    <>
+      <section className={styles.grid}>
+        <div>
+          <h1 className={styles.title}>Hi, I’m Julian, Web developer.</h1>
+        </div>
+        <img
+          alt="julian "
+          width="206"
+          src="https://avatars.githubusercontent.com/u/16819318?v=4"
+        />
+        <div></div>
+      </section>
+    </>
   );
 };
 
