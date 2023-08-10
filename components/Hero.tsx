324db52fa7ee435c31b2a162a717a971b83af77b
Julian Aijal <julian@thenextweb.com>
2023-08-10 20:14:11 +0200

    position hero elements

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,20 +5,22 @@ import myIllustration from '../public/assets/julian.svg';
 const Hero = () => {
   return (
     <>
-      <section className={styles.grid}>
-        <div>
+      <section className={styles.HeroGrid}>
+        <div className={styles.Hero__cta}>
           <h1 className={styles.title}>Hi, I’m Julian, Web developer.</h1>
         </div>
-        <Image
-          alt="julian aijal"
-          src={myIllustration}
-          priority={true}
-          sizes="100vw"
-          style={{
-            width: '100%',
-            height: 'auto',
-          }}
-        />
+        <div className={styles.Hero__visual}>
+          <Image
+            alt="julian aijal"
+            src={myIllustration}
+            priority={true}
+            sizes="100vw"
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+          />
+        </div>
       </section>
     </>
   );
