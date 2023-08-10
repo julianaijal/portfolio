4ceac0178ad2a0ed32592dc380a93d2754cc30dc
Julian Aijal <julian@thenextweb.com>
2023-08-10 13:17:03 +0200

    Add seperate componenet for hero

---
+++ pages/components/Hero.tsx
@@ -0,0 +1,10 @@
+import styles from '../../styles/Home.module.css'
+const Hero = () => {
+  return (
+    <h1 className={styles.title}>
+    Welcome to <a href="#">julianaijal.com</a>
+  </h1>
+  );
+};
+
+export default Hero;
