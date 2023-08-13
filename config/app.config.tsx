9db27dcb8d7ce48ef41164746132421727524fb1
Julian Aijal <julian@thenextweb.com>
2023-08-13 22:48:35 +0200

    add analytics in separate config file

---
+++ config/app.config.tsx
@@ -0,0 +1,13 @@
+type AppConfig = {
+  env: {
+    analyticsMeasurementId?: string;
+  };
+};
+
+const appConfig: AppConfig = {
+  env: {
+    analyticsMeasurementId: process.env.ANALYTICS_MEASUREMENT_ID,
+  },
+};
+
+export default appConfig;
