f90644b24e5fba480010d3aee6c386616a78e6b5
Julian Aijal <julian@thenextweb.com>
2023-08-15 11:17:32 +0200

    add logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,30 @@
 import styles from './../styles/Navbar.module.scss';
+import Image from 'next/image';
+import myLogo from '../public/assets/julian-aijal-logo.svg';
+
 const NavBar = () => {
   return (
     <>
-      <div>
-        <a>logo</a>
-      </div>
-      <div>
-        <ul className={styles.nav}>
-          <li>lorem ipsum</li>
-          <li>lorem ipsum</li>
-          <li>lorem ipsum</li>
-        </ul>
-      </div>
+      <nav>
+        <div>
+          <Image
+            alt="julian aijal"
+            src={myLogo}
+            sizes="100vw"
+            style={{
+              width: '100%',
+              height: '2rem',
+            }}
+          />
+        </div>
+        <div>
+          <ul className={styles.nav}>
+            <li>lorem ipsum</li>
+            <li>lorem ipsum</li>
+            <li>lorem ipsum</li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 };
