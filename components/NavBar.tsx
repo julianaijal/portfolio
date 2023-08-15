f6d299eae3e1f4a742fc0c89af92ca831a0a4c0a
Julian Aijal <julian@thenextweb.com>
2023-08-15 15:50:19 +0200

    remove redundant fragment

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,30 +5,26 @@ import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <>
-      <nav className={styles.nav}>
-        <div className={styles.nav__logo}>
-          <Image
-            alt="julian aijal"
-            src={myLogo}
-            sizes="100vw"
-            style={{
-              width: '100%',
-              height: '2rem',
-            }}
-          />
-        </div>
-        <div className={styles.nav__menu}>
-          <ul className={styles.nav__list}>
-            <li className={styles.nav__listItem}>
-              <Link href="mailto:hello@julianaijal.com">
-                Ping me!
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
+    <nav className={styles.nav}>
+      <div className={styles.nav__logo}>
+        <Image
+          alt="julian aijal"
+          src={myLogo}
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: '2rem',
+          }}
+        />
+      </div>
+      <div className={styles.nav__menu}>
+        <ul className={styles.nav__list}>
+          <li className={styles.nav__listItem}>
+            <Link href="mailto:hello@julianaijal.com">Ping me!</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
