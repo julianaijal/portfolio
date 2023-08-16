d3e4306c78ac14e58ff91257ec4ab95a885d7066
Julian Aijal <julian@thenextweb.com>
2023-08-16 11:56:54 +0200

    add social icons to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,10 @@
 import styles from './../styles/Footer.module.scss';
 import Logo from '../public/assets/julian-aijal-logo.svg';
 import Icon from '../public/assets/icons/envelope.svg';
+import github from '../public/assets/icons/github.svg';
+import linkedin from '../public/assets/icons/linkedin.svg';
+import twitter from '../public/assets/icons/twitter.svg';
+import instagram from '../public/assets/icons/instagram.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -31,10 +35,39 @@ const Footer = () => {
         />
       </div>
       <div className={styles.footer__social}>
-        <ul>
-          <li>Link</li>
-          <li>Link</li>
-          <li>Link</li>
+        <ul className={styles.footer__socialList}>
+          <li className={styles.footer__socialItem}>
+            <Image
+              alt="julian aijal"
+              src={github}
+              sizes="100vw"
+              style={{ width: '100%', height: '2rem' }}
+            />
+          </li>
+          <li className={styles.footer__socialItem}>
+            <Image
+              alt="julian aijal"
+              src={linkedin}
+              sizes="100vw"
+              style={{ width: '100%', height: '2rem' }}
+            />
+          </li>
+          <li className={styles.footer__socialItem}>
+            <Image
+              alt="julian aijal"
+              src={twitter}
+              sizes="100vw"
+              style={{ width: '100%', height: '2rem' }}
+            />
+          </li>
+          <li className={styles.footer__socialItem}>
+            <Image
+              alt="julian aijal"
+              src={instagram}
+              sizes="100vw"
+              style={{ width: '100%', height: '2rem' }}
+            />
+          </li>
         </ul>
       </div>
     </footer>
