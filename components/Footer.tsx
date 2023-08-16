22d0a8ba0e0cebe21204c45603fd6b9dcfd1190a
Julian Aijal <julian@thenextweb.com>
2023-08-16 12:55:16 +0200

    position contact footer element

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,15 +12,6 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__contact}>
-        <Image
-          alt="julian aijal"
-          src={Icon}
-          sizes="100vw"
-          style={{
-            width: '100%',
-            height: '2rem',
-          }}
-        />
         <Link href="mailto:hello@julianaijal.com">Ping me!</Link>
       </div>
       <div className={styles.footer__logo}>
