98830c6e1e936d3af5933d9599cace0bb1b1aa0d
Julian Aijal <julian@thenextweb.com>
2023-08-16 11:04:44 +0200

    add contact elements to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,24 @@
 import styles from './../styles/Footer.module.scss';
 import myLogo from '../public/assets/julian-aijal-logo.svg';
+import  Icon from '../public/assets/envelope.svg';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footer__contact}>Contact</div>
+      <div className={styles.footer__contact}>
+      <Image
+          alt="julian aijal"
+          src={Icon}
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: '2rem',
+          }}
+        />
+      <Link href="mailto:hello@julianaijal.com">Ping me!</Link>
+      </div>
       <div className={styles.footer__logo}>
         <Image
           alt="julian aijal"
