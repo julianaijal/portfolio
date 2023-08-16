1ab029af3e4f37d8ce892ea2926b5d0e1b018597
Julian Aijal <julian@thenextweb.com>
2023-08-16 10:58:14 +0200

    add logo in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,31 @@
 import styles from './../styles/Footer.module.scss';
+import myLogo from '../public/assets/julian-aijal-logo.svg';
+import Image from 'next/image';
 
 const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.footer__contact}>Contact</div>
-            <div className={styles.footer__logo}>logo</div>
-            <div className={styles.footer__social}>
-                <ul>
-                    <li>
-                        Link
-                    </li>
-                    <li>
-                        Link
-                    </li>
-                    <li>
-                        Link
-                    </li>
-                </ul>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__contact}>Contact</div>
+      <div className={styles.footer__logo}>
+        <Image
+          alt="julian aijal"
+          src={myLogo}
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: '2rem',
+          }}
+        />
+      </div>
+      <div className={styles.footer__social}>
+        <ul>
+          <li>Link</li>
+          <li>Link</li>
+          <li>Link</li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
