4d62b6a64cca7f543335dca7d622f6afc9ec2414
Julian Aijal <julian@thenextweb.com>
2023-09-05 12:56:19 +0200

    update prisma client

--- app/_lib/prisma.tsx
+++ app/_lib/prisma.tsx
@@ -1,18 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-declare global {
-  var prisma: PrismaClient | any;
-}
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+if (process.env.NODE_ENV != "production") globalForPrisma.prisma;
 
-export default prisma;
