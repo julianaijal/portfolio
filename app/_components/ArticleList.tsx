a298a10a1bc65deb55074d9abdb80b9e9e40421a
Julian Aijal <julian@thenextweb.com>
2023-09-10 14:59:42 +0200

    map article entries

--- app/_components/ArticleList.tsx
+++ app/_components/ArticleList.tsx
@@ -1,24 +1,30 @@
 import styles from '../styles/ArticleList.module.scss';
-interface ArticleListProps  {
-articles: {
-  id: string
-  title: string
-  substitle?: string
-  image?: string,
-  cta: string,
-  url: string 
-}[];
+interface ArticleListProps {
+  articles: {
+    id: string;
+    title: string;
+    substitle?: string;
+    image?: string;
+    cta: string;
+    url: string;
+  }[];
 }
-const ArticleList:  React.FC<ArticleListProps> = ({articles}) => {
+const ArticleList: React.FC<ArticleListProps> = ({ articles }) => {
   return (
     <>
       <section className={styles.ArticleList}>
         <div className={styles.ArticleListCta}>
           <h2 className={styles.ArticleListCtaHeading}>Articles</h2>
-          <p className={styles.ArticleListCtaCopy}>Some articles I have written</p>
+          <p className={styles.ArticleListCtaCopy}>
+            Some articles I have written
+          </p>
         </div>
         <div className={styles.ArticleListEntries}>
-          <p>list</p>
+          <ul>
+            {articles.map((article, index) => (
+              <li key={index}>{article.title}</li>
+            ))}
+          </ul>
         </div>
       </section>
     </>
