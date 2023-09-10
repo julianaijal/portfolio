a7bc989efaef0d33b6d68c12326d5ad4cb0063ff
Julian Aijal <julian@thenextweb.com>
2023-09-10 15:04:05 +0200

    add prop types for article slider

--- app/_components/ArticleSlider.tsx
+++ app/_components/ArticleSlider.tsx
@@ -1,5 +1,17 @@
 import styles from '../styles/ArticleSlider.module.scss';
-const ArticleSlider = () => {
+
+interface ArticleSlideProps {
+  articles: {
+    id: string;
+    title: string;
+    substitle?: string;
+    image?: string;
+    cta: string;
+    url: string;
+  }[];
+}
+
+const ArticleSlider: React.FC<ArticleSlideProps> = (articles) => {
   return <h2 className={styles.heading}>Article Slider</h2>;
 };
 
