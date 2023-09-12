d09bbfaa0c95533e90be176d3b15e2184af90dab
Julian Aijal <julian@thenextweb.com>
2023-09-12 13:39:32 +0200

    add card elements

--- app/_components/ArticleSlider.tsx
+++ app/_components/ArticleSlider.tsx
@@ -1,5 +1,7 @@
 import styles from '../styles/ArticleSlider.module.scss';
 import { IArticles } from './_interfaces/interfaces';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const ArticleSlider: React.FC<IArticles> = ({ articles }) => {
   console.log(articles);
@@ -14,7 +16,25 @@ const ArticleSlider: React.FC<IArticles> = ({ articles }) => {
       <div className={styles.ArticleSliderEntries}>
         {articles.map((article, index) => (
           <div className={styles.ArticleSliderCard} key={index}>
-            <p className={styles.ArticleSliderCardTitle}>{article.title}</p>
+            <div className={styles.ArticleSliderCardCta}>
+              <Image
+                alt={article.title}
+                src={`/assets/icons/${article.image}.svg`}
+                sizes="100vw"
+                width={48}
+                height={48}
+              />
+              <Link
+                rel="noopener"
+                href={article.url}
+                className={styles.ArticleListItemLink}
+              >
+                {article.cta}
+              </Link>
+            </div>
+            <div className={styles.ArticleSliderCardCopy}>
+              <h2>{article.title}</h2>
+            </div>
           </div>
         ))}
       </div>
