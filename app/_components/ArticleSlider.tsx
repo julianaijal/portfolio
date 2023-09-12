cee93477309b1dd76d4fbd40840826d5072bcb4c
Julian Aijal <julian@thenextweb.com>
2023-09-12 11:15:30 +0200

    add article slider elements

--- app/_components/ArticleSlider.tsx
+++ app/_components/ArticleSlider.tsx
@@ -4,11 +4,21 @@ import { IArticles } from './_interfaces/interfaces';
 const ArticleSlider: React.FC<IArticles> = ({ articles }) => {
   console.log(articles);
   return (
-    <>
-      {articles.map((article, index) => (
-        <li key={index}>{article.title}</li>
-      ))}
-    </>
+    <section className={styles.ArticleSlider}>
+      <div className={styles.ArticleSliderCta}>
+        <h2 className={styles.ArticleSliderCtaHeading}>Articles</h2>
+        <p className={styles.ArticleSliderCtaCopy}>
+          Some articles I have written
+        </p>
+      </div>
+      <div className={styles.ArticleSliderEntries}>
+        <ul>
+          {articles.map((article, index) => (
+            <li key={index}>{article.title}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
