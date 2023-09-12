21cd8053c45eb7edea9c8a82e2a2efd446323d5c
Julian Aijal <julian@thenextweb.com>
2023-09-12 13:20:01 +0200

    remove redundant fragemnt

--- app/_components/ArticleList.tsx
+++ app/_components/ArticleList.tsx
@@ -5,47 +5,43 @@ import Link from 'next/link';
 
 const ArticleList: React.FC<IArticles> = ({ articles }) => {
   return (
-    <>
-      <section className={styles.ArticleList}>
-        <div className={styles.ArticleListCta}>
-          <h2 className={styles.ArticleListCtaHeading}>Articles</h2>
-          <p className={styles.ArticleListCtaCopy}>
-            Some articles I have written
-          </p>
-        </div>
-        <div className={styles.ArticleListEntries}>
-          <ul>
-            {articles.map((article, index) => (
-              <li key={index}>
-                <div className={styles.ArticleListItem}>
-                  <div className={styles.ArticleListItemImage}>
-                    <Image
-                      alt={article.title}
-                      src={`/assets/icons/${article.image}.svg`}
-                      sizes="100vw"
-                      width={48}
-                      height={48}
-                    />
-                  </div>
-                  <h3 className={styles.ArticleListItemTitle}>
-                    {article.title}
-                  </h3>
-                  <div className={styles.ArticleListItemCta}>
-                    <Link
-                      rel="noopener"
-                      href={article.url}
-                      className={styles.ArticleListItemLink}
-                    >
-                      {article.cta}
-                    </Link>
-                  </div>
+    <section className={styles.ArticleList}>
+      <div className={styles.ArticleListCta}>
+        <h2 className={styles.ArticleListCtaHeading}>Articles</h2>
+        <p className={styles.ArticleListCtaCopy}>
+          Some articles I have written
+        </p>
+      </div>
+      <div className={styles.ArticleListEntries}>
+        <ul>
+          {articles.map((article, index) => (
+            <li key={index}>
+              <div className={styles.ArticleListItem}>
+                <div className={styles.ArticleListItemImage}>
+                  <Image
+                    alt={article.title}
+                    src={`/assets/icons/${article.image}.svg`}
+                    sizes="100vw"
+                    width={48}
+                    height={48}
+                  />
                 </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
-    </>
+                <h3 className={styles.ArticleListItemTitle}>{article.title}</h3>
+                <div className={styles.ArticleListItemCta}>
+                  <Link
+                    rel="noopener"
+                    href={article.url}
+                    className={styles.ArticleListItemLink}
+                  >
+                    {article.cta}
+                  </Link>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
