a41c95dae4a835902ee662445b75533cd23b3fb9
Julian Aijal <julian.aijal@gmail.com>
2024-08-09 19:34:16 +0200

    add hygraph to nextjs config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['placehold.co'],
+    domains: ['placehold.co', 'eu-central-1-shared-euc1-02.graphassets.com'],
   },
 }
 
