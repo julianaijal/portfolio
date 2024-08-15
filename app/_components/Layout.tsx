0b4c8fd0bb27857ee0a9d1fd47fb34cf464cf914
Julian Aijal <julian.aijal@gmail.com>
2024-08-15 15:40:45 +0200

    format and optimise layout component

--- app/_components/Layout.tsx
+++ app/_components/Layout.tsx
@@ -1,31 +1,26 @@
 import styles from "../styles/Home.module.scss";
 import { ArticleBlock, Hero, NavBar, Footer } from "./";
-// import { getShowcase } from "./../_lib/prisma";
 import fetchPosts from "../utils/api";
 import { IArticle } from "./_interfaces/interfaces";
 
 const Layout = async () => {
-  const [articlesHygraph] = await Promise.all([
-    fetchPosts(),
-    // getShowcase(),
-  ]);
+  const [articlesHygraph] = await Promise.all([fetchPosts()]);
 
   const externalPosts: IArticle[] =
     articlesHygraph?.data?.externalPostsPluralized ?? [];
   const articlesHygraphData: IArticle[] = externalPosts.map(
-    ({ title, url, symbol }) => ({
-      title,
-      url,
-      symbol,
+    (post: IArticle) => ({
+      title: post.title,
+      url: post.url,
+      symbol: post.symbol,
     })
   );
-  // const showcaseData = entries.map(({ title }) => ({ title }));
+
   return (
     <>
       <NavBar />
       <main className={styles.main}>
         <Hero />
-        {/* <Showcase entries={showcaseData} /> */}
         <ArticleBlock articles={articlesHygraphData} />
       </main>
       <Footer />
