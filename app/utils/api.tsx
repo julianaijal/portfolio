697073b25e6e050d04c8b28703471624e6f42af2
Julian Aijal <julian.aijal@gmail.com>
2024-08-15 17:49:43 +0200

    add on-demand revalidation to article fetch

--- app/utils/api.tsx
+++ app/utils/api.tsx
@@ -1,6 +1,6 @@
+"use server";
 const fetchPosts = async () => {
   const api = `https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/${process.env.HYGRAPH_API_KEY}/master`;
-
   try {
     const response = await fetch(api, {
       method: "POST",
@@ -14,6 +14,7 @@ const fetchPosts = async () => {
           }
         }`,
       }),
+      next: { tags: ["external-articles-collection"] },
     });
 
     if (!response.ok) {
