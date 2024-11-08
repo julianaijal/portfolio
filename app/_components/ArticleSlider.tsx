8afda6e7b6adf7b27fbeea3101468e5be20b4339
Julian Aijal <julian.aijal@gmail.com>
2024-11-08 14:12:09 +0100

    add temp buttons to go to new dynamic route

--- app/_components/ArticleSlider.tsx
+++ app/_components/ArticleSlider.tsx
@@ -5,6 +5,7 @@ import { IArticles } from './_interfaces/interfaces';
 import { FC } from 'react';
 
 const ArticleSlider: FC<IArticles> = ({ articles }) => {
+  
   return (
     <section className={styles.ArticleSlider}>
       <div className={styles.ArticleSliderCta}>
@@ -14,41 +15,47 @@ const ArticleSlider: FC<IArticles> = ({ articles }) => {
         </p>
       </div>
       <div className={styles.ArticleSliderEntries}>
-        {articles.map((article, index) => (
-          <div className={styles.ArticleSliderEntry} key={index}>
-            <div className={styles.ArticleSliderCard}>
-              <div className={styles.ArticleSliderCardCta}>
-                <div className={styles.ArticleSliderCardImg}>
-                  {article.symbol?.url && (
-                    <Image
-                      alt={article.title}
-                      src={article.symbol.url}
-                      sizes="100vw"
-                      width={48}
-                      height={48}
-                    />
-                  )}
-                </div>
-                <div className={styles.ArticleSliderCardBtnWrapper}>
-                  <div className={styles.ArticleSliderCardBtn}>
-                    <Link
-                      rel="noopener"
-                      href={article.url}
-                      className={styles.ArticleSliderCardLink}
-                    >
-                      Read more
+        {articles.map((article, index) => {
+          const slug = article.title.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '');
+          return (
+            <div className={styles.ArticleSliderEntry} key={index}>
+              <div className={styles.ArticleSliderCard}>
+                <div className={styles.ArticleSliderCardCta}>
+                  <div className={styles.ArticleSliderCardImg}>
+                    {article.symbol?.url && (
+                      <Image
+                        alt={article.title}
+                        src={article.symbol.url}
+                        sizes="100vw"
+                        width={48}
+                        height={48}
+                      />
+                    )}
+                  </div>
+                  <div className={styles.ArticleSliderCardBtnWrapper}>
+                    <div className={styles.ArticleSliderCardBtn}>
+                      <Link
+                        rel="noopener"
+                        href={article.url}
+                        className={styles.ArticleSliderCardLink}
+                      >
+                        Read more
+                      </Link>
+                    </div>
+                    <Link href={`/articles/${slug}`} passHref>
+                      <button className={styles.ArticleListItemButton}>Go</button>
                     </Link>
                   </div>
                 </div>
-              </div>
-              <div className={styles.ArticleSliderCardCopy}>
-                <h3 className={styles.ArticleSliderCardTitle}>
-                  {article.title}
-                </h3>
+                <div className={styles.ArticleSliderCardCopy}>
+                  <h3 className={styles.ArticleSliderCardTitle}>
+                    {article.title}
+                  </h3>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </section>
   );
