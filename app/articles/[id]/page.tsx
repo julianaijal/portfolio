063ea14eecfea42484cd92fb26eeffaa37e504e1
Julian Aijal <julian.aijal@gmail.com>
2024-11-08 14:32:45 +0100

    import empty styles module

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { NavBar, Footer } from "./../../_components";
+import styles from '../../styles/Article.module.scss';
 const Page = ({ params }: { params: { id: string } }) => {
   return (
     <>
     <NavBar/>
-      <h1>{params.id}</h1>
+      <h1 className={styles.ArticleTitle}>{params.id}</h1>
     <Footer />
     </>
   );
