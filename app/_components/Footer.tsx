f520a4244bb33dfac814fe1008f42f424d5c11aa
Julian Aijal <julian.aijal@gmail.com>
2024-11-20 21:52:51 +0100

    Move social to separate cons for better readability

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,62 +1,70 @@
-import styles from './../styles/Footer.module.scss';
-import Logo from '../../public/assets/julian-aijal-logo.svg';
-import Image from 'next/image';
-import Link from 'next/link';
+import styles from "./../styles/Footer.module.scss";
+import Logo from "../../public/assets/julian-aijal-logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+const socialLinks = [
+  {
+    href: "https://github.com/julianaijal",
+    src: "/assets/icons/github.svg",
+    title: "GitHub",
+  },
+  {
+    href: "https://www.linkedin.com/in/jaijal/",
+    src: "/assets/icons/linkedin.svg",
+    title: "LinkedIn",
+  },
+  {
+    href: "https://x.com/Jaijal",
+    src: "/assets/icons/x.svg",
+    title: "Twitter (X)",
+  },
+  {
+    href: "https://instagram.com/julian.aijal",
+    src: "/assets/icons/instagram.svg",
+    title: "Instagram",
+  },
+];
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__contact}>
-        <Link href="mailto:hello@julianaijal.com">Ping me!</Link>
+        <Link href="mailto:hello@julianaijal.com" aria-label="Email Julian Aijal">
+          Ping me!
+        </Link>
       </div>
+
       <div className={styles.footer__logo}>
         <Image
-          alt="julian aijal"
+          alt="Julian Aijal logo"
           src={Logo}
           sizes="100vw"
+          priority
           style={{
-            width: '100%',
-            height: '2rem',
+            width: "100%",
+            height: "2rem",
           }}
         />
       </div>
-      <div className={styles.footer__social}>
+
+      <nav className={styles.footer__social} aria-label="Social Media Links">
         <ul className={styles.footer__socialList}>
-          {[
-            {
-              href: 'https://github.com/julianaijal',
-              src: '/assets/icons/github.svg',
-              title:'github',
-            },
-            {
-              href: 'https://www.linkedin.com/in/jaijal/',
-              src: '/assets/icons/linkedin.svg',
-              title:'linkedin',
-            },
-            {
-              href: 'https://x.com/Jaijal',
-              src: '/assets/icons/x.svg',
-              title:'X',
-            },
-            {
-              href: 'https://instagram.com/julian.aijal',
-              src: '/assets/icons/instagram.svg',
-              title:'instagram',
-            },
-          ].map((social, index) => (
+          {socialLinks.map(({ href, src, title }, index) => (
             <li className={styles.footer__socialItem} key={index}>
-              <Link rel="noopener" href={social.href}>
+              <Link href={href} rel="noopener noreferrer" aria-label={title}>
                 <Image
-                  src={social.src}
-                  alt={social.title}
+                  src={src}
+                  alt={title}
                   height={32}
                   width={32}
+                  loading="lazy"
                 />
               </Link>
             </li>
           ))}
         </ul>
-      </div>
+      </nav>
     </footer>
   );
 };
