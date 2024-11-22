2024b82fdbbf12d43d09117190a4f8eab22b4f2b
Julian Aijal <julian.aijal@gmail.com>
2024-11-22 18:13:51 +0100

    render props

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -3,22 +3,42 @@ import styles from "../../styles/Article.module.scss";
 import apiFunctions from "../../utils/api";
 
 interface PageProps {
-  params: Promise<{ slug: string }>;
+  params: { slug: string };
 }
 
 const Page = async ({ params }: PageProps) => {
-  const resolvedParams = await params;
-  const slug = resolvedParams.slug;
-  const data = await apiFunctions.fetchArticleBySlug(slug);
-  console.log(data);
+  const { slug } = params;
+  try {
+    const data = await apiFunctions.fetchArticleBySlug(slug);
+    // console.log(data);
+    const content = data.content.html;
 
-  return (
-    <>
-      <NavBar />
-      <h1 className={styles.ArticleTitle}>{slug}</h1>
-      <Footer />
-    </>
-  );
+    return (
+      <>
+        <NavBar />
+        <main className={styles.Article}>
+          <h1 className={styles.ArticleTitle}>{data?.title}</h1>
+          <article>{content}</article>
+        </main>
+        <Footer />
+      </>
+    );
+  } catch (error) {
+    console.error("Error fetching article:", error);
+
+    return (
+      <>
+        <NavBar />
+        <main className={styles.Article}>
+          <h1 className={styles.ArticleTitle}>Error</h1>
+          <p className={styles.ArticleContent}>
+            Could not fetch the article. Sad.
+          </p>
+        </main>
+        <Footer />
+      </>
+    );
+  }
 };
 
 export default Page;
