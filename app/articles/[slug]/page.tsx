ed195afca91d3e91f580317eadcbb5d84cc4746a
Julian Aijal <julian.aijal@gmail.com>
2024-11-22 18:32:25 +0100

    add html with some safety stuff

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,24 +1,32 @@
 import { NavBar, Footer } from "../../_components";
 import styles from "../../styles/Article.module.scss";
 import apiFunctions from "../../utils/api";
+import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
 
 interface PageProps {
   params: { slug: string };
 }
 
 const Page = async ({ params }: PageProps) => {
-  const { slug } = params;
+  const { slug } = await params;
   try {
     const data = await apiFunctions.fetchArticleBySlug(slug);
-    // console.log(data);
+
     const content = data.content.html;
+    const window = new JSDOM("").window;
+    const domPurify = DOMPurify(window);
+    const sanitizedHtml = domPurify.sanitize(content);
 
     return (
       <>
         <NavBar />
         <main className={styles.Article}>
           <h1 className={styles.ArticleTitle}>{data?.title}</h1>
-          <article>{content}</article>
+          <article
+            className={styles.ArticleContent}
+            dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
+          />
         </main>
         <Footer />
       </>
