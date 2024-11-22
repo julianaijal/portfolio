687d3800e623bac65ab2b53e4dcec53639b04b93
Julian Aijal <julian.aijal@gmail.com>
2024-11-22 17:48:49 +0100

    pass slug to create dynamic page

--- app/_components/ArticleListEntry.tsx
+++ app/_components/ArticleListEntry.tsx
@@ -1,40 +1,28 @@
-import styles from '../styles/ArticleList.module.scss';
-import Image from 'next/image';
-import Link from 'next/link';
-import { IArticle } from './_interfaces/interfaces';
-import { FC } from 'react';
+import styles from "../styles/ArticleList.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import { IArticle } from "./_interfaces/interfaces";
+import { FC } from "react";
 
-const ArticleListEntry: FC<IArticle> = ({ title, url, symbol }) => {
-  // to do 1: fetch slugs from hygraph entries from parent, deconstruct above (see next comment)
-  const slug = title.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '');
-
+// add symbol field in hygraph
+const ArticleListEntry: FC<IArticle> = ({ title, slug }) => {
   return (
     <li className={styles.ArticleListItem}>
-      {symbol?.url && (
-        <div className={styles.ArticleListItemImage}>
-          <Image
-            alt={title}
-            src={symbol.url}
-            sizes="100vw"
-            width={48}
-            height={48}
-          />
-        </div>
-      )}
+      {/* {symbol?.url && ( */}
+      <div className={styles.ArticleListItemImage}>
+        <Image alt={title} src="" sizes="100vw" width={48} height={48} />
+      </div>
+      {/* )} */}
       <h3 className={styles.ArticleListItemTitle}>{title}</h3>
       <div className={styles.ArticleListItemCta}>
         <Link
           rel="noopener"
-          href={url}
+          href={`/articles/${slug}`}
           className={styles.ArticleListItemLink}
         >
           Read more
         </Link>
       </div>
-      {/* to do 2: and pass here */}
-      <Link href={`/articles/${slug}`} passHref>
-        <button className={styles.ArticleListItemButton}>go</button>
-      </Link>
     </li>
   );
 };
