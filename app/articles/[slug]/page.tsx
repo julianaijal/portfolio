be62cbb0dda71b9369fb214c1502fa525a2fcb13
Julian Aijal <julian.aijal@gmail.com>
2025-02-01 22:19:07 +0100

    add canonical link if applicable

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -7,6 +7,23 @@ import { IArticleDynamic } from "../../_components/_interfaces/interfaces";
 import Image from "next/image";
 import { Metadata } from 'next';
 
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+  const { slug } = params;
+  const data = await apiFunctions.fetchArticleBySlug(slug);
+
+  const metadata: Metadata = {
+    title: data?.title,
+    description: data?.subtitle || '',
+  };
+
+  if (data?.canonicalReference && data?.canonicalLink) {
+    metadata.alternates = {
+      canonical: data.canonicalLink
+    };
+  }
+
+  return metadata;
+}
 
 const Page = async ({ params }: IArticleDynamic) => {
   const resolvedParams = await params;
