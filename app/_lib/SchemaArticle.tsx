256f3df4390566d2aacebbeded7e5964e73f0384
Julian Aijal <julian.aijal@gmail.com>
2025-02-10 19:03:10 +0100

    create empty article schema file

---
+++ app/_lib/SchemaArticle.tsx
@@ -0,0 +1,15 @@
+const SchemaArticle = () => {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: `[{
+          "@context":"https://schema.org",
+          
+       }]`,
+      }}
+    />
+  );
+};
+
+export default SchemaArticle;
